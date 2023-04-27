d0ed07d4d57c42336618e30de7902b5d8e3ef6b6
Gustavo Becattini <80481089+gusbecattini@users.noreply.github.com>
2023-04-27 16:50:05 -0300

    Add files via upload

---
+++ CodeConnect/static/code/post.js
@@ -0,0 +1,39 @@
+function bold() {
+    // Faz o texto ficar em negrito
+
+    document.getElementById("textarea1").style.fontWeight = "bold";
+}
+
+function italic() {
+    //transforma o texto em itálico
+
+    document.getElementById("textarea1").style.fontStyle = "italic";
+}
+
+function left() {
+    //Alinha o texto à esquerda
+
+    document.getElementById("textarea1").style.textAlign = "left";
+}
+
+function center() {
+    //Centraliza o texto
+
+    document.getElementById("textarea1").style.textAlign = "center";
+}
+
+function right() {
+    //Alinha o texto à direita
+
+    document.getElementById("textarea1").style.textAlign = "right";
+}
+
+function clear() {
+    //Limpa os inputs do usuário na caixa de texto
+
+    document.getElementById("textarea1").style.fontWeight = "normal";
+    document.getElementById("textarea1").style.textAlign = "left";
+    document.getElementById("textarea1").style.fontStyle = "normal";
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+    document.getElementById("textarea1").value = " ";
+}
